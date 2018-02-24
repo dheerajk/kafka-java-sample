603dcdc39570c1955321493807d03a5da5316874
dheerajkarande <dheeraj.karande@squareoneinsights.com>
2018-02-24 16:29:57 +0530

    Added simple kafka producer

---
+++ src/main/java/com/example/kafka/config/ProducerProperties.java
@@ -0,0 +1,22 @@
+package com.example.kafka.config;
+
+import java.util.Properties;
+
+import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
+
+final public class ProducerProperties {
+
+    private static final String HOST = "localhost:9092";
+    private static final String KEY_SERIALIZER = "org.apache.kafka.common.serialization.ByteArraySerializer";
+    private static final String VALUE_SERIALIZER = "org.apache.kafka.common.serialization.StringSerializer";
+
+    public static Properties get() {
+        Properties configProperties = new Properties();
+        configProperties.put(BOOTSTRAP_SERVERS_CONFIG, HOST);
+        configProperties.put(KEY_SERIALIZER_CLASS_CONFIG, KEY_SERIALIZER);
+        configProperties.put(VALUE_SERIALIZER_CLASS_CONFIG, VALUE_SERIALIZER);
+        return configProperties;
+    }
+}
